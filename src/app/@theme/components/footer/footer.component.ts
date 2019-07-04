0a44a97c48d0cb2c1e7e36d4c266d55a862b3081
Shweta Vaidya <shweta@edulab.in>
2019-07-04 17:20:21 +0530

    Changes in Footer.

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="http://edulab.in" target="_blank">Edulab</a></b> 2019</span>
+    <span class="created-by">Created with ♥</span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
